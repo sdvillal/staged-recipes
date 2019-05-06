2639efeb0bfb1dc6ecac4a9ca4f4e609a2280578
Mark Harfouche <mark.harfouche@gmail.com>
2019-05-06 10:00:29 -0400

    Not too sure. This is probably wrong

--- recipes/libusb1-devel-cos6-x86_64/build.sh
+++ recipes/libusb1-devel-cos6-x86_64/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -ex
+
 RPM=$(find ${PWD}/binary -name "*.rpm")
 mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot
-pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
-  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
-popd > /dev/null 2>&1
+pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot
+cpio -idmv ${RECIPE_DIR}/binary
+popd
