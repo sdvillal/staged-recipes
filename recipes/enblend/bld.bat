0cff28b7f9e0cf91dc4f7d54c1cede6cab0eaf5c
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-12 13:52:29 -0400

    close but blockign on gsl not being compiled correctly.

--- recipes/enblend/bld.bat
+++ recipes/enblend/bld.bat
@@ -1,9 +1,7 @@
 mkdir build
 cd build
 
-cmake                                                                     ^
-    -LAH                                                                  ^
-    -G Ninja                                                              ^
+cmake -LAH -G Ninja                                                       ^
     -DCMAKE_BUILD_TYPE="Release"                                          ^
     -DCMAKE_PREFIX_PATH=${LIBRARY_PREFIX}                                 ^
     -DCMAKE_INSTALL_PREFIX=${LIBRARY_PREFIX}                              ^
